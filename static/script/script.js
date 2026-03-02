ab369d754e8c385d2095f5e11b825518fbc02d92
luckymessi321 <luckymessi321@gmail.com>
2026-03-02 15:37:31 +0100

    la til kode som slutter spillet når du kolliderer med en vegg eller deg selv. la også til epler som kan spises for å bli lengre og få score

--- static/script/script.js
+++ static/script/script.js
@@ -5,6 +5,13 @@ let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
 let gridWidth = 10
+let appleIndex = 0
+let count = 0
+
+function updateScore() {
+    count++
+    if (score) score.textContent = `${count}`;
+}
 
 function createGrid() {
     //lager 100 av firkantene med en for-løkke
@@ -24,6 +31,16 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        (currentSnake[0] >= 90 && currentSnake[0] <= 100 && direction === 10) || // om snake head er på bunnen av grid
+        (currentSnake[0] >= 0 && currentSnake[0] <= 9 && direction === -10) || // om snake head er på toppen av grid
+        (currentSnake[0] % 10 === 0 && direction === -1) || // om snake head er ved venstre vegg av grid
+        (currentSnake[0] % 10 === 9 && direction === 1) || // om snake head er ved høyre vegg av grid
+        squares[currentSnake[0] + direction].classList.contains('snake') // om snake crasher i seg selv
+    ) {
+        clearInterval(timerId)
+        direction = 0
+    }
     //fjerner siste element/firkant i currentSnake tabellen
     const tail = currentSnake.pop()
     //fjerner styling fra siste element/firkant
@@ -31,10 +48,35 @@ function move() {
     //legger til firkanter i retningen slangen flytter seg
     currentSnake.unshift(currentSnake[0] + direction)   
     //legger til styling til de nye firkantene
+    // bestemmer hva som skjer om snake spiser et eple
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        //fjerner klassen til eplet slik at det ser ut som om det ble spist
+        squares[currentSnake[0]].classList.remove('apple')
+        //øker snake lengde
+        squares[tail].classList.add('snake')
+        console.log(tail)
+        //legger til en ny firkant i currentSnake slik at halen (tail) følger resten av snake
+        currentSnake.push(tail)
+        console.log(currentSnake)
+        //lager et nytt eple
+        generateApple()
+        //legger til 1 til scoren
+        updateScore()
+    }
+
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 const timerId = setInterval(move, 1000)
+
+function generateApple () {
+    do {
+        // generer tilfeldig tall
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple') 
+}
+generateApple()
 
 function control(e) {
   // Use string values for the 'e.key' property for modern compatibility
