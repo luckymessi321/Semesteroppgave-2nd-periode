85cd9eba246be709e85388a5d9fc15fec8c799bf
luckymessi321 <luckymessi321@gmail.com>
2026-03-06 09:28:29 +0000

    tilpasset filene jeg importerte. detaljert forklaring ligger i changelog.txt

--- Semesteroppgave-2nd-periode/app.py
+++ Semesteroppgave-2nd-periode/app.py
@@ -1,4 +1,4 @@
-# koden for funksjonen get_conn() og rutene ('/register') og ('/login') er limt inn fra en lærer sin presentasjon 
+# koden for funksjonen get_conn() og rutene ('/register') og ('/login') er limt inn fra en lærer sin presentasjon, men tilpasset prosjektet mitt. alle kommentarer er skrevet av meg unless stated otherwise
 from flask import Flask, render_template, redirect, session
 import mysql.connector
 from forms import RegisterForm, LoginForm
@@ -6,12 +6,12 @@ from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
-# Enkel DB-tilkobling
+# funksjon som oppretter en tilkobling til databasen 'login' med brukeren 'lukas'
 def get_conn():
     return mysql.connector.connect(
         host="localhost",
-        user="hanna",
-        password=" ",
+        user="lukas",
+        password="messi123",
         database="login"
     )
 
@@ -19,50 +19,62 @@ def get_conn():
 def index():
     return render_template('index.html')
 
+# rute med kode som kjører KUN når register.html henter data fra app.py (GET), eller sender data til app.py (POST)
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # oppretter et nytt objekt 'form' av 'RegisterForm' klassen importert fra forms.py
     form = RegisterForm()
+    # kode som KUN kjører når brukeren sender inn data (submit)
     if form.validate_on_submit():
-        navn = form.name.data
-        brukernavn = form.username.data
-        passord = form.password.data
-        adresse = form.address.data
+        # definerer brukernavn og passord i variabler
+        username = form.username.data
+        password = form.password.data
 
+        # oppretter tilkobling til MySQL databasen og lagrer tilkoblingen som en variabel 'conn'. dette lar meg bruke methods som .commit()
         conn = get_conn()
+        # oppretter et cursor objekt 'cur' koblet til databasen. dette objektet kan kjøre MySQL kode på serveren.
         cur = conn.cursor()
+        # kjører MySQL kode på serveren som lagrer brukernavn og passord hentet fra '/register' ruten i databasen.
         cur.execute(
-            "INSERT INTO brukere (navn, brukernavn, passord, adresse) VALUES (%s, %s, %s, %s)",
-            (navn, brukernavn, passord, adresse)
+            "INSERT INTO users (username, password) VALUES (%s, %s)",
+            (username, password)
         )
+        # lagrer endringene gjort i cur.execute og lukker tilkoblingen til databasen
         conn.commit()
         cur.close()
         conn.close()
+        # sender brukeren til login siden
         return redirect("/login")
 
     return render_template("register.html", form=form)
 
+# rute med kode som kjører KUN når login.html henter data fra app.py (GET), eller sender data til app.py (POST)
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        brukernavn = form.username.data
-        passord = form.password.data
+        username = form.username.data
+        password = form.password.data
 
         conn = get_conn()
         cur = conn.cursor()
+        # kjører MySQL kode på serveren som henter brukernavnet til brukeren spilleren logget inn med
         cur.execute(
-            "SELECT navn FROM brukere WHERE brukernavn=%s AND passord=%s",
-            (brukernavn, passord)
+            "SELECT username FROM users WHERE username=%s AND password=%s",
+            (username, password)
         )
+        # lagrer brukernavnet som variablen 'user'
         user = cur.fetchone()
         cur.close()
         conn.close()
-
+        
+        # kode som KUN kjører om spillerens brukernavn eksisterer
         if user:
-            session['navn'] = user[0]  # lagrer navnet i session
-            return redirect("/welcome")
+            session['username'] = user[0]  # lagrer brukernavn i session-data
+            return redirect("/")
+        # returnerer feilmelding om brukernavn ikke finnes
         else:
-            form.username.errors.append("Feil brukernavn eller passord")
+            form.username.errors.append("Invalid username or password")
 
     return render_template("login.html", form=form)
 
--- Semesteroppgave-2nd-periode/forms.py
+++ Semesteroppgave-2nd-periode/forms.py
@@ -1,15 +1,13 @@
-# denne filen er limt inn fra en lærer sin presentasjon
+# denne filen er limt inn fra en lærer sin presentasjon, men tilpasset til prosjektet mitt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
 
+# oppretter 2 nye klasser; 'RegisterForm' og 'LoginForm
 class RegisterForm(FlaskForm):
     username = StringField("Brukernavn", validators=[InputRequired()])
     password = PasswordField("Passord", validators=[InputRequired()])
-    name = StringField("Navn", validators=[InputRequired()])
-    address = StringField("Adresse", validators=[InputRequired()])
     submit = SubmitField("Registrer")
-
 class LoginForm(FlaskForm):
     username = StringField("Brukernavn", validators=[InputRequired()])
     password = PasswordField("Passord", validators=[InputRequired()])
