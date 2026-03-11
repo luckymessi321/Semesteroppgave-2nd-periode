dc2c255bb31bfbf26fbc300857c83f8e6a88969a
luckymessi321 <luckymessi321@gmail.com>
2026-03-11 08:54:41 +0000

    la til funksjonen 'confirmEdit' i manage.js og oppdaterte kode under edit user formen (se changelog)

--- Semesteroppgave-2nd-periode/static/script/manage.js
+++ Semesteroppgave-2nd-periode/static/script/manage.js
@@ -1,3 +1,8 @@
 function confirmLogout () {
     return confirm("Are you sure that you want to log out of your account?")
 }
+
+function confirmEdit () {
+    return confirm("Are you sure you want to edit your account?")
+}
+
